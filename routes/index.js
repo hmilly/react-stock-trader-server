b55a23c2691ebb0b8a4e80e4a0d03f4f768c8cec
hmilly <milstowers@gmail.com>
2021-09-27 15:31:51 +0100

    working to set /post route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 router.get("/", (req, res) => {
   res.send({ response: "I'm Alive" }).status(200);
 });
 
-router.get("/user", (req, res) => {
-  res.send({ response: "Hi" }).status(200);
+router.post("/", (req, res) => {
+
+  try { fs.writeFileSync(`./data.js`, { ...req.body }) }
+  catch (error) { console.log(error) }
+
+  res.send(req.body).status(200)
+  return
 });
+
+
+
 
 
 module.exports = router;
